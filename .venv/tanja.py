88f986231ca706d30df025422aca5f60b5cb236d
Dmitry P <dimitry2809p@gmail.com>
2025-03-15 15:02:05 +0100

    Create Templates Base.html index.html

--- .venv/tanja.py
+++ .venv/tanja.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from flask import render_template
 
 app= Flask(__name__)
+
+menuUp = ["Головна", "Питання", "Регiстрацiя"]
+
+@app.route("/")
+def index():
+    return render_template("index.html", titel = "головна", menu = menuUp)
 
 if __name__ == "__main__":
     app.run(debug=True)
